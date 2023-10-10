fe6f918651104898799fa46e840ac2b46043f3f2
juampa95 <jpmanzano95@gmail.com>
2023-10-10 09:53:40 -0300

    Modificacion de index.py

--- portfolio_web/pages/index.py
+++ portfolio_web/pages/index.py
@@ -1,18 +1,9 @@
 import reflex as rx
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from portfolio_web.base_state import State
 from portfolio_web.components.navbar import navbar
 from portfolio_web.styles import styles
-
-top_box = rx.flex(rx.square('',
-                            bg='black',
-                            width='70%'),
-                  rx.square('',
-                            bg='green',
-                            width='30%'),
-                  h='300px'
-                  )
 
 grilla = rx.grid(
     rx.grid_item(row_span=2, col_span=14, bg='#224040',),
@@ -46,12 +37,62 @@ p1 = rx.box(rx.text('Hola, soy:', color='white'),
             rx.text('Cientifico de datos JR', color='white', fontSize='xl')
             )
 
-p2 = rx.hstack(rx.box(rx.image(src='/perfilcfondo.png'),
-                   width='30%', align='center', justify='center'),
-               rx.card('el texto del about me',
-                       header=rx.text('About Me'),
-                       footer=rx.text('Boton de CV y email'),
-                       bg='#345959'),
+def delta_time(desde):
+    dif = datetime.now() - datetime.strptime(desde, "%Y-%m-%d")
+    anios = int(dif.days / 365)
+    meses = int(dif.days % 365)//30
+    if anios == 0:
+        if meses == 0:
+            return 'nada'
+        elif meses == 1:
+            return f'{meses} mes'
+        else:
+            return f'{meses} meses'
+    elif anios == 1:
+        if meses == 0:
+            return f'{anios} año'
+        elif meses == 1:
+            return f'{anios} año y {meses} mes'
+        else:
+            return f'{anios} año y {meses} meses'
+    else:
+        if meses == 0:
+            return f'{anios} años'
+        elif meses == 1:
+            return f'{anios} años y {meses} mes'
+        else:
+            return f'{anios} años y {meses} meses'
+
+
+bio = rx.box(rx.text('Profesional data-driven orientado a la '
+              'resolución de problemas mediante innovación continua' ),
+             rx.divider(width=0),
+             rx.text(f'Desde hace {delta_time("2022-08-19")} trabajo como ingeniero'
+                     f'en el departamento tecnico de una empresa local realizando diferentes'
+                     f'tareas orientadas a la gestion de proyectos e innovacion de equipos'
+                     ,font_size='0.8em'),
+             rx.text(f'Llevo {delta_time("2021-10-10")} capacitandome en Python, Data y '
+                     f'Machine-learning ', font_size='0.8em'))
+
+p2 = rx.hstack(rx.box(rx.image(src='/perfilrecorte.png'),
+                      width='25%', align='center', justify='center',
+                      boxShadow="5px 5px 10px rgba(0, 0, 0, 0.2), -5px -5px 10px rgba(11, 111, 111, 0.8)"
+                      ),
+               rx.container(rx.heading('About Me'),
+                            rx.divider(width='15%',
+                                       variant='solid'),
+                            bio,
+                            rx.stack(
+                                rx.button('Descargar CV', bg='#BAD9D6'),
+                                rx.button('Enviar email', bg='#BAD9D6'),
+                                direction='row',
+                                justify='center',
+                                margin_top='5px'
+                            ),
+                            ),
+               left='20%',
+               position='relative',
+               width='70%'
                )
 
 flex = rx.box(
@@ -66,10 +107,11 @@ flex = rx.box(
             height='40%',
             width='100%',
             position='relative'),
-    rx.flex(rx.flex(p2,bg='#345959', width='100%'),
-            height='40%'),
+    rx.flex(rx.flex(p2,bg='#345959', width='100%',
+                    left='20%'),
+            height='45%'),
     rx.flex(rx.box(bg='#224040', width='100%'),
-            height='20%',),
+            height='15%',),
     width='100%',
     h='100vh'
 
